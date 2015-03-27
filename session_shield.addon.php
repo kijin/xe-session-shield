964c1bdbfdd3779022220d4d72194ec906974217
Kijin Sung <kijin@kijinsung.com>
2015-03-27 21:06:10 +0900

    Add comments describing the CSRF protection

--- session_shield.addon.php
+++ session_shield.addon.php
@@ -8,8 +8,11 @@
  * 
  * This addon improves session security by preventing XSS, session fixation,
  * and some types of sniffing attacks when SSL is only partially used.
+ * It also adds a randomly generated token to every web page,
+ * and checks the token whenever a POST request is made.
+ * This helps prevent CSRF attacks.
  * 
- * This addon is experimental. It may cause problems in XE 1.6 ~ 1.7.7.2,
+ * This addon is experimental. It may cause problems in older versions of XE,
  * and when XE is used with SSO and/or virtual websites.
  */
 
